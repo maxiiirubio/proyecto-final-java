fcc4afca735b1e115ae2997518c333f95955a347
Maxi Rubio <maxirubio92@gmail.com>
2022-09-21 22:39:18 -0300

    proyecto final

---
+++ codigo.js
@@ -0,0 +1,143 @@
+const divProductos = document.getElementById("divProductos")
+const divCarrito = document.getElementById("divCarrito")
+const btnCarrito = document.getElementById("btnCarrito")
+const contador = document.getElementById("contador")
+const finalizar = document.getElementById("btnFinalizar")
+
+const cargarProductos = async () => {
+    const response = await fetch('/productos.json')
+    const productos = await response.json()
+    return productos
+}
+
+cargarProductos().then(productos => {
+    productos.forEach((producto, indice) => {
+        divProductos.innerHTML+= `
+        <div class="card productos" id="producto${indice}" style="width: 25rem;">
+            <img src="/imagenes/${producto.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${producto.nombre}</h5>
+                <p class="card-text">Marca: ${producto.marca}</p>
+                <p class="card-text">Precio: $${producto.precio}</p>
+                <p class="card-text">Stock: ${producto.stock}</p>
+                <p class="card-text">Id: ${producto.id}</p>
+                <button class="btn btn-primary">Agregar al carrito</button>
+            </div>
+        </div>
+        `
+    })
+})
+
+
+
+let carrito = []
+let compraFinal = []
+
+
+
+if(localStorage.getItem('carrito')) {
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+} else{
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+if(localStorage.getItem('compra')) {
+    compraFinal = JSON.parse(localStorage.getItem('compra'))
+} else{
+    localStorage.setItem('compra', JSON.stringify(compraFinal))
+}
+
+cargarProductos().then(productos => {
+    productos.forEach((producto, indice) => {
+        const cardProducto = document.getElementById(`producto${indice}`)
+    
+        cardProducto.children[1].children[5].addEventListener('click', () => {
+            Toastify({
+				text: `${producto.nombre} ${producto.marca} agregado al carrito`,
+				duration: 3000,
+				// destination: "https://github.com/apvarun/toastify-js",
+				// newWindow: true,
+				close: false,
+				gravity: "bottom", // `top` or `bottom`
+				position: "right", // `left`, `center` or `right`
+				stopOnFocus: true, // Prevents dismissing of toast on hover
+				style: {
+				  background: "linear-gradient(to right, #350b5c, #000000)",
+				  color: "#ffffff"
+				},
+				onClick: function(){} // Callback after click
+			  }).showToast();
+            if(carrito.find(prod => prod.id == producto.id)) {
+                let index = carrito.findIndex((prod => prod.id == producto.id))
+                if(carrito[index].cantidad < producto.stock){
+                    carrito[index].cantidad++
+                }
+            } else {
+                const prodCarrito = {id: producto.id, nombre: producto.nombre, marca: producto.marca, precio: producto.precio, cantidad: producto.cantidad, img: producto.img}
+                carrito.push(prodCarrito)
+            }
+
+            contador.innerText = carrito.reduce((acc, prod) => acc + prod.precio * prod.cantidad, 0)
+    
+            localStorage.setItem('carrito', JSON.stringify(carrito))
+    
+    })
+    })
+})
+
+
+btnCarrito.addEventListener('click', () => {
+    const verProdCarrito = JSON.parse(localStorage.getItem('carrito'))
+    
+    divCarrito.innerHTML= ""
+    verProdCarrito.forEach((producto, indice) => {
+        divCarrito.innerHTML += `
+        <div class="card" id="producto${indice}" style="width: 18rem;">
+            <img src="/imagenes/${producto.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${producto.nombre}</h5>
+                <p class="card-text">Marca: ${producto.marca}</p>
+                <p class="card-text">Precio: $${producto.precio}</p>
+                <p class="card-text">Cantidad: ${producto.cantidad}</p>
+                <p class="card-text">Id: ${producto.id}</p>
+                <button class="btn btn-danger">Eliminar Producto</button>
+            </div>
+        </div>
+        `
+    })
+    verProdCarrito.forEach((producto, indice) => {
+        const borrarElemnto = document.getElementById(`producto${indice}`)
+        
+        borrarElemnto.children[1].children[5].addEventListener('click', () => {
+            borrarElemnto.remove()
+            carrito.splice(indice, 1)
+            if(borrarElemnto == true){
+                contador.innerText = carrito.reduce((acc, prod) => acc - prod.precio * prod.cantidad, 0) 
+            } else {
+                contador.innerText = carrito.reduce((acc, prod) => acc + prod.precio * prod.cantidad, 0)
+            }
+
+            
+            localStorage.setItem("carrito", JSON.stringify(carrito))
+            
+        })
+    })
+    verProdCarrito.forEach((producto, indice) => {
+        const borrarElemnto = document.getElementById(`producto${indice}`)
+        
+        finalizar.addEventListener('click', () => {
+            Swal.fire({
+                icon: 'success',
+                title: 'COMPRA FINALIZADA'
+            })
+            const productoCarrito = {id: producto.id, nombre: producto.nombre, marca: producto.marca, precio: producto.precio, cantidad: producto.cantidad}
+            compraFinal.push(productoCarrito)
+            localStorage.setItem('compra', JSON.stringify(compraFinal))
+            contador.innerText = "" 
+            borrarElemnto.remove()
+            carrito.splice(indice, 1)
+            localStorage.setItem("carrito", JSON.stringify(carrito))
+        })
+    })
+})
+
+
